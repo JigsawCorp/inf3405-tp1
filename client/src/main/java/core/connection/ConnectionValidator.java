1450aa615f57da2d0be0f79eded3f8bd8a51d7e0
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-09-05 12:28:49 -0400

    Added Common/ConnectionValidator

---
+++ client/src/main/java/core/connection/ConnectionValidator.java
@@ -0,0 +1,4 @@
+package core.connection;
+
+public class ConnectionValidator {
+}
---
+++ common/src/main/java/connection/ConnectionValidator.java
@@ -0,0 +1,40 @@
+package connection;
+
+public class ConnectionValidator {
+    private static final int minPort = 5000;
+    private static final int maxPort = 5050;
+    private static final int maxIpByteVal = 255;
+    private static final int minIpByteVal = 0;
+
+    public boolean validateIp(String IP)
+    {
+        String[] ipBytes = IP.split(".");
+
+        // Return false if less than 4 bytes
+        if (ipBytes.length != 4) {
+            return false;
+        }
+
+        for (int i = 0; i < ipBytes.length; ++i) {
+            int byteVal;
+
+            try {
+                byteVal = Integer.parseInt(ipBytes[i]);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+
+            if (byteVal < minIpByteVal || byteVal > maxIpByteVal) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean validatePort(int port)
+    {
+        return port >= minPort && port <= maxPort;
+    }
+
+}
