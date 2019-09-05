46ab94fc9aaa14973ec8ed281910a877f3a59459
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-09-05 13:31:58 -0400

    Added Application.java.

--- client/src/main/java/application/Application.java
+++ client/src/main/java/application/Application.java
@@ -1,4 +1,12 @@
 package application;
 
+import core.connection.ConnectionCreator;
+import core.connection.ConnectionHandler;
+
 public class Application {
+    public static void main(String[] args)
+    {
+        ConnectionHandler connectionHandler = ConnectionCreator.createConnectionHandler();
+        while(true){}
+    }
 }
