2d6c045e66547714f29adabdd23394d778b5ccad
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-09-11 12:28:57 -0400

    Added exception handling to application.java.

--- client/src/main/java/application/Application.java
+++ client/src/main/java/application/Application.java
@@ -3,9 +3,16 @@ package application;
 import core.connection.ConnectionCreator;
 import core.connection.ConnectionHandler;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class Application {
-    public static void main(String[] args)
+    public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException
     {
+        Path path = FileSystems.getDefault().getPath(".").toAbsolutePath();
+        System.out.println(path.toString());
         ConnectionHandler connectionHandler = ConnectionCreator.createConnectionHandler();
         while(true){}
     }
