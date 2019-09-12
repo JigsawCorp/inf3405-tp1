f26154909506b80c61c3aa997d250f4176ce7173
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-09-12 15:49:52 -0400

    Created Message.Info class.

--- common/src/main/java/communication/Info.java
+++ common/src/main/java/communication/Info.java
@@ -1,4 +1,19 @@
 package communication;
 
-public class Info {
+import java.io.Serializable;
+
+public class Info extends Message implements Serializable {
+    private String fMessage;
+
+    public Info(String message)
+    {
+        super(Type.INFO);
+        fMessage = message;
+    }
+
+    @Override
+    public String toString()
+    {
+        return fMessage;
+    }
 }
