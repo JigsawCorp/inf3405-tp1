6b011b3c0f5c731cb4a47ab88ddd77b0b82d9e98
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-09-12 15:52:51 -0400

    Added exception handling to ConnectionHandler.

--- client/src/main/java/core/connection/ConnectionHandler.java
+++ client/src/main/java/core/connection/ConnectionHandler.java
@@ -1,90 +1,122 @@
 package core.connection;
 
 
+import communication.Message;
+
 import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.nio.file.FileAlreadyExistsException;
 
 public class ConnectionHandler {
     private ServerInformation fServerInfo;
-    private Socket fSocket;
+    private Socket fSocket = null;
+    private static ConnectionHandler sInstance = null;
 
-    public ConnectionHandler(ServerInformation serverInfo) throws UnknownHostException, IOException, ClassNotFoundException
+    public static ConnectionHandler getInstance()
+    {
+        if (sInstance == null) {
+            sInstance = new ConnectionHandler();
+        }
+
+        return ConnectionHandler.sInstance;
+
+    }
+
+    void connectToServer(ServerInformation serverInfo) throws IOException
     {
         fServerInfo = serverInfo;
-        fSocket = null;
-        connectToServer();
-    }
-
-    public void connectToServer() throws UnknownHostException, IOException, ClassNotFoundException
-    {
-        try {
-            // Cr�ation d'un socket client vers le serveur. Ici 127.0.0.1 est indicateur que
-            // le serveur s'ex�cute sur la machine locale. Il faut changer 127.0.0.1 pour
-            // l'adresse IP du serveur si celui-ci ne s'ex�cute pas sur la m�me machine. Le port est 5000.
-            fSocket = new Socket(fServerInfo.fIPAdress, fServerInfo.fPort);
-            ObjectOutputStream objectOutput = new ObjectOutputStream(fSocket.getOutputStream());
-            // Ici, on suppose que le fichier que vous voulez inverser se nomme text.txt
-            List<String> linesToSend = readFile("text.txt");
-            // �criture de l'objet � envoyer dans le output stream. Attention, la fonction
-            // writeObject n'envoie pas l'objet vers le serveur! Elle ne fait qu'�crire dans
-            // le output stream.
-            objectOutput.writeObject(linesToSend);
-            // Envoi des lignes du fichier texte vers le serveur sous forme d'une liste.
-            objectOutput.flush();
-            // Cr�ation du input stream, pour recevoir les donn�es trait�es du serveur.
-            ObjectInputStream obj = new ObjectInputStream(fSocket.getInputStream());
-            // Not� bien que la fonction readObject est bloquante! Ainsi, l'ex�cution du
-            // client s'arr�te jusqu'� la r�ception du r�sultat provenant du serveur!
-            Stack<String> receivedStack = (Stack<String>) obj.readObject();
-            // �criture du r�sultat dans un fichier nomm�e FichierInversee.txt
-            writeToFile(receivedStack, "FichierInversee.txt");
-        } finally {
-            // Fermeture du socket.
-            //clientSocket.close();
+        if (fSocket != null && fSocket.isConnected()) {
+            closeConnection();
         }
+        fSocket = new Socket(fServerInfo.fIPAdress, fServerInfo.fPort);
     }
 
-    private void closeConnection() throws IOException
+    public void closeConnection() throws IOException
     {
         fSocket.close();
     }
 
-    // Fonction permettant de lire un fichier et de stocker son contenu dans une liste.
-    private static List<String> readFile(String nomFichier) throws IOException {
-        List<String> listOfLines = new ArrayList<String>();
-        String line = null;
-        FileReader fileReader = null;
-        BufferedReader bufferedReader = null;
+    public void sendMessage(Message message) throws IOException
+    {
+        ObjectOutputStream outStream = null;
+
         try {
-            fileReader = new FileReader(nomFichier);
-
-            bufferedReader = new BufferedReader(fileReader);
-
-            while ((line = bufferedReader.readLine()) != null) {
-                listOfLines.add(line);
-            }
+            outStream = new ObjectOutputStream(fSocket.getOutputStream());
+            outStream.writeObject(message);
+        } catch (IOException e) {
+            System.out.println("Erreur lors de l'envoi d'une message au serveur! Erreur complète:");
+            System.out.println(e.toString());
         } finally {
-            fileReader.close();
-            bufferedReader.close();
+            if (outStream != null) outStream.flush();
         }
-        return listOfLines;
     }
 
-    // Fonction permettant d'�crire dans un fichier les donn�es contenues dans la
-    // stack re�u du serveur.
-    private static void writeToFile(Stack<String> myStack, String nomFichier) throws IOException {
-        BufferedWriter out = null;
+    public Message waitForMessage() throws IOException
+    {
+        ObjectInputStream inStream = null;
         try {
-            out = new BufferedWriter(new FileWriter(nomFichier));
-            while (!myStack.isEmpty()) {
-                out.write(myStack.pop() + "\n");
-            }
+            inStream = new ObjectInputStream(fSocket.getInputStream());
+            return (Message) inStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Erreur lors de la réception d'une réponse du serveur! Erreur complète:");
+            System.out.println(e.toString());
+            return null;
         } finally {
-            out.close();
+            //if (inStream != null) inStream.close();
+        }
+    }
+
+    public void sendFile(String fileName) throws IOException
+    {
+        InputStream inStream = null;
+        OutputStream outStream = null;
+
+        try {
+            File file = new File(fileName);
+            byte[] buffer = new byte[8192];
+            inStream = new FileInputStream(file);
+            outStream = fSocket.getOutputStream();
+
+            int count;
+            while ((count = inStream.read(buffer)) > 0) {
+                outStream.write(buffer, 0, count);
+            }
+        } catch (IOException e) {
+            System.out.println("Erreur lors de l'envoi du fichier! Erreur complète:");
+            System.out.println(e.toString());
+        } finally {
+            if (outStream != null) outStream.flush();
+            //if (inStream != null) inStream.close();
+        }
+    }
+
+    public void downloadFile(String filePath) throws IOException
+    {
+        InputStream inStream = null;
+        FileOutputStream outStream = null;
+
+        try {
+            inStream = fSocket.getInputStream();
+
+            File f = new File(filePath);
+            if (f.exists()) {
+                throw new FileAlreadyExistsException(filePath + " already exists!");
+            }
+
+            outStream = new FileOutputStream(filePath);
+
+            byte[] bytes = new byte[8192];
+
+            int count;
+            while ((count = inStream.read(bytes)) > 0) {
+                outStream.write(bytes, 0, count);
+            }
+        } catch (IOException e) {
+            System.out.println("Erreur lors de la réception du fichier! Erreur complète:");
+            System.out.println(e.toString());
+        } finally {
+            if (outStream != null) outStream.close();
+            //if (inStream != null) inStream.close();
         }
     }
 
