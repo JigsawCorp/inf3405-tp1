22d47d760b0234933793db71906c9c03290c154c
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-10-13 16:02:10 -0400

    Added CD command on server.

---
+++ server/src/main/java/core/command/CD.java
@@ -0,0 +1,21 @@
+package core.command;
+
+import command.Command;
+import core.CommunicationHandler;
+
+import java.io.IOException;
+
+public class CD extends CommandHandler {
+
+    public CD(Command command, CommunicationHandler communicationHandler) {
+        super(command, communicationHandler);
+    }
+
+    @Override
+    public void execute(String currentWorkingDirectory) throws IOException
+    {
+        sendCommand(fCommand);
+        handleResponse();
+    }
+
+}
