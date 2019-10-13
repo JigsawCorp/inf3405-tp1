d535d226ac0d848cb7849c9f48d0389e824583f1
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-10-13 19:20:56 -0400

    Added MKDIR on server.

--- server/src/main/java/core/command/MKDIR.java
+++ server/src/main/java/core/command/MKDIR.java
@@ -1,4 +1,24 @@
 package core.command;
 
-public class MKDIR {
+import command.Command;
+import core.ClientConnection;
+import core.CommunicationHandler;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class MKDIR extends CommandHandler {
+
+    public MKDIR(Command command, ClientConnection clientConnection) {
+        super(command, clientConnection);
+    }
+
+    @Override
+    public void execute(Path currentWorkingDirectory) throws IOException
+    {
+
+        sendMessage(fCommand);
+        handleResponse();
+    }
+
 }
