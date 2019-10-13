5551da38b72a8d01c93424a5acc9d7a51526dca5
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-10-13 19:21:20 -0400

    Added FileUtils.

--- server/src/main/java/utils/FileUtils.java
+++ server/src/main/java/utils/FileUtils.java
@@ -1,4 +1,19 @@
 package utils;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileUtils {
+    public static boolean checkPathExists(String newPath, Path currentWorkingDirectory)
+    {
+        Path path = currentWorkingDirectory.resolve(newPath).normalize();
+
+        return Files.exists(path);
+    }
+
+    public static Path getCombinedPath(String newPath, Path currentWorkingDirectory)
+    {
+        return currentWorkingDirectory.resolve(newPath).normalize();
+    }
 }
