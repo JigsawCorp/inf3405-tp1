6813790f73448a6cdb5e789934ffa3851f11ebb4
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-10-13 16:02:31 -0400

    LS now shows files and folders on the server.

--- server/src/main/java/core/command/LS.java
+++ server/src/main/java/core/command/LS.java
@@ -1,15 +1,35 @@
 package core.command;
 
 import command.Command;
+import core.CommunicationHandler;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class LS extends CommandHandler {
 
+    public LS(Command command, CommunicationHandler communicationHandler) {
+        super(command, communicationHandler);
+    }
+
     @Override
-    public void execute(Command command) throws IOException
+    public void execute(String currentWorkingDirectory) throws IOException
     {
-        sendCommand(command);
+        Path base = Paths.get(currentWorkingDirectory);
+        File f = new File(currentWorkingDirectory);
+        File[] files = f.listFiles();
+        for (int i = 0; i < files.length; ++i) {
+            if (files[i].isDirectory()) {
+                System.out.println("[Folder] " + base.relativize(files[i].toPath()).toString());
+            } else {
+                System.out.println("[File] " + base.relativize(files[i].toPath()).toString());
+            }
+        }
+        //System.out.println(base.relativize(Paths.get(Arrays.toString(f.listFiles())));
+        sendCommand(fCommand);
         handleResponse();
     }
 
