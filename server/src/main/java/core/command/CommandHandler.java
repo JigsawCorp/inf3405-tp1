3263c87dc050a966e075d53d272da13f1ca1adeb
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-10-13 16:03:20 -0400

    Fixed constructor of CommandHandler.

--- server/src/main/java/core/command/CommandHandler.java
+++ server/src/main/java/core/command/CommandHandler.java
@@ -8,13 +8,26 @@ import java.io.IOException;
 
 public abstract class CommandHandler {
     private CommunicationHandler fCommunicationHandler;
+    protected Command fCommand;
 
-    public CommandHandler(CommunicationHandler communicationHandler)
+    protected CommandHandler(Command command, CommunicationHandler communicationHandler)
     {
         fCommunicationHandler = communicationHandler;
     }
 
-    public abstract void execute(Command command) throws Exception;
+    public static CommandHandler instantiate(Command command, CommunicationHandler communicationHandler)
+    {
+        switch (command.fCommandName) {
+            case CD:
+                return new CD(command, communicationHandler);
+            case LS:
+                return new LS(command, communicationHandler);
+            default:
+                return null;
+        }
+    }
+
+    public abstract void execute(String currentWorkingDirectory) throws Exception;
 
     void sendCommand(Command command) throws IOException
     {
