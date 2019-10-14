887bd1bbba816d574dc7c9edfd907dd205803cca
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-10-13 21:45:12 -0400

    Client waits for ack when uploading.

--- client/src/main/java/core/command/handlers/Upload.java
+++ client/src/main/java/core/command/handlers/Upload.java
@@ -1,6 +1,8 @@
 package core.command.handlers;
 
 import command.Command;
+import communication.Info;
+import communication.Message;
 
 import java.io.IOException;
 
@@ -10,7 +12,10 @@ public class Upload extends CommandHandler {
     public void execute(Command command) throws IOException
     {
         sendCommand(command);
-        uploadFile(command.fArguments[0]);
-        handleResponse();
+        Info message = (Info) waitForMessage();
+        if (message.fInfoType == Info.InfoType.ACK && message.fStatus == Info.Status.SUCCESS) {
+            uploadFile(command.fArguments[0]);
+            handleResponse();
+        }
     }
 }
