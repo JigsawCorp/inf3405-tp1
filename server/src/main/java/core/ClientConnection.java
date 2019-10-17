40f0199525662f932c5cc732f690a8e403a41251
Philippe Côté-Morneault <philippe.cote-morneault@polymtl.ca>
2019-10-17 16:22:10 -0400

    Added comments.

--- server/src/main/java/core/ClientConnection.java
+++ server/src/main/java/core/ClientConnection.java
@@ -8,12 +8,14 @@ import java.net.Socket;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
+
 public class ClientConnection implements Runnable {
-    private Socket fSocket = null;
-    private Thread fThread;
     public CommunicationHandler fCommunicationHandler;
-    private boolean fAcceptClientCommunication;
     public Path fCurrentWorkingDirectory;
+
+    private Socket fSocket;
+    private Thread fThread;
+    private boolean fAcceptClientCommunication;
 
     public ClientConnection(Socket socket) {
         fSocket = socket;
@@ -25,31 +27,32 @@ public class ClientConnection implements Runnable {
 
     @Override
     public void run() {
-        try {
-            enableIncomingCommunication();
-        } catch (Exception e) {
-
-        }
+        enableIncomingCommunication();
     }
 
+    // Start the listening loop of client messages
     public void enableIncomingCommunication()
     {
         if (!fAcceptClientCommunication) {
             fAcceptClientCommunication = true;
             while (fAcceptClientCommunication) {
                 Message message = fCommunicationHandler.receiveMessage();
-                CommandLogger.logCommand((Command) message, fSocket);
-                CommandHandler command = CommandHandler.instantiate(((Command) message), this);
-                try {
-                    command.execute(fCurrentWorkingDirectory);
-                } catch (Exception e) {
+                if (message.dType == Message.Type.COMMAND) {
+                    CommandLogger.logCommand((Command) message, fSocket);
+                    CommandHandler command = CommandHandler.instantiate(((Command) message), this);
 
+                    try {
+                        command.execute(fCurrentWorkingDirectory);
+                    } catch (Exception e) {
+
+                    }
                 }
             }
         }
     }
 
-    public void disableIncomingCommnication()
+    // Stop listening to incoming messages
+    public void disableIncomingCommunication()
     {
         fAcceptClientCommunication = false;
     }
